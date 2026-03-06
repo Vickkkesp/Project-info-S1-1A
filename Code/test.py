111b5c8d140b8b434378e0f145f0554a31d45ae3
Vickkkesp <vespinozaaaaa@gmail.com>
2026-03-06 23:57:10 +0100

    modification importante hyperlinks

--- Code/test.py
+++ Code/test.py
@@ -11,12 +11,39 @@ app = Flask(__name__)
 
 @app.route("/")
 def page_html():
- return render_template("index1.html",param = s)
+ return render_template("Acueil.html")
+
+@app.route("/page0")
+def page0_html():
+ return render_template("Conexion.html")
+
+@app.route("/page02")
+def page02_html():
+ return render_template("autentification.html")
+
+@app.route("/page01")
+def page01_html():
+ return render_template("creation_compte.html")
+
+@app.route("/page03")
+def page03_html():
+ return render_template("deconextion.html")
 
 @app.route("/page1")
 def page1_html():
- return render_template("index2.html",param = s)
+ return render_template("Produits.html")
 
+@app.route("/page2")
+def page2_html():
+ return render_template("produits_f.html")
+
+@app.route("/page3")
+def page3_html():
+ return render_template("Produits_h.html")
+
+@app.route("/page4")
+def page4_html():
+ return render_template("Promotions.html")
 
 if __name__ == '__main__':
  app.run(debug=True)
