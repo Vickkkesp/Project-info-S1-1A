91c016d40e59b2f4ed90a005b0b8e1f6fe1a5ea0
Vickkkesp <vespinozaaaaa@gmail.com>
2026-03-15 22:23:03 +0100

    creation stats et graphiques

---
+++ Code/stats BD.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+def get_db():
+    conn = sqlite3.connect("ProjectBdd.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+import matplotlib.pyplot as plt
+from db import get_db
+
+def graphique_utilisateurs():
+    conn = get_db()
+    db= Projet_Bdd.db()
+
+    rows= db.execute("""
+SELECT v.date_vente AS jour,
+                     SUM(b.quantite*l.prix_unitaire) AS chiffre_affaires
+                     FROM vente v
+                     JOIN ligne:vente l ON v.id = l.vente_id
+                     GROUP BY
+                     ORDER BY
+                     """)
+
+    cursor.execute("SELECT strftime('%Y-%m', date_inscription) AS mois, COUNT(*) AS nombre_utilisateurs FROM utilisateurs GROUP BY mois")
+    resultats = cursor.fetchall()
+
+    mois = [row['mois'] for row in resultats]
+    nombre_utilisateurs = [row['nombre_utilisateurs'] for row in resultats]
+
+    plt.bar(mois, nombre_utilisateurs)
+    plt.xlabel('Mois')
+    plt.ylabel('Nombre d\'utilisateurs')
+    plt.title('Nombre d\'utilisateurs inscrits par mois')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
